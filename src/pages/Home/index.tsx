2d1e519a020463c04041d787eeeb47337fea0e57
ademarCardoso <ac@storyblok.com>
2020-06-05 19:49:49 -0300

    Big bang and removing unnecessary files

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Home.css'
+import logo from '../../assets/logo.svg'
+import { FiLogIn } from 'react-icons/fi'
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Logo do ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem poontos de coleta de forma eficiente.</p>
+
+          <a href="/cadastro">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Home
