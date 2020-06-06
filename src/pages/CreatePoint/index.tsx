3d150706f409eb54f48af3d29ecf4c81e0885eb8
ademarCardoso <ac@storyblok.com>
2020-06-06 00:18:42 -0300

    add history router and submit content to api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, ChangeEvent } from 'react'
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'
 import './CreatePoint.css'
 import logo from '../../assets/logo.svg'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import { LeafletMouseEvent } from 'leaflet'
@@ -36,6 +36,8 @@ const CreatePoint = () => {
     whatsapp: ''
   })
   const [selectedItems, setSelectedItems] = useState<number[]>([])
+
+  const history = useHistory()
 
   useEffect(() => {
     api.get('items')
@@ -110,6 +112,32 @@ const CreatePoint = () => {
     }
   }
 
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const { name, email, whatsapp } = formData
+    const uf = selectedUf
+    const city = selectedCity
+    const [latitude, longitude] = selectedPosition
+    const items = selectedItems
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    }
+
+    await api.post('points', data)
+    
+    alert("Ponto criado")
+    history.push('/')
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -121,7 +149,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form >
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
 
         <fieldset>
